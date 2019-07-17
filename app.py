ad9abdb650632b6a2077944b7db66322c53ea717
wuhongyi <wuhongyi>
2019-07-17 17:01:28 +0800

    Create,edit and delete item by form

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import sys
 import click
 from flask_sqlalchemy import SQLAlchemy
-from flask import url_for
+from flask import url_for,request,redirect,flash
 from flask import Flask, render_template
 
 WIN = sys.platform.startswith('win')
@@ -34,10 +34,48 @@ def inject_user():
     return dict(user=user)
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        year = request.form.get('year')
+        if not title or not year or len(year)>4 or len(title)>60:
+            flash('无效输入')
+            return redirect(url_for('index'))
+        movie = Movie(title=title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('提交成功！')
+        return redirect(url_for('index'))
     movies = Movie.query.all()
     return render_template('index.html', movies=movies)
+
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET','POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    if request.method == 'POST':
+        title = request.form.get('title')
+        year = request.form.get('year')
+        if not title or not year or len(year)>4 or len(title)>60:
+            flash('无效输入')
+            return redirect(url_for('edit',movie_id=movie_id))
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('更新成功！')
+        return redirect(url_for('index'))
+    return render_template('edit.html', movie=movie)
+
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('删除成功！')
+    return redirect(url_for('index'))
+
 
 @app.route('/user/<name>')
 def user_page(name):
